375c2a16698cf5022619759eb975d615f31e8c8e
euphmat <euphmat@gmail.com>
2020-08-26 15:01:36 +0900

    勤務情報欄 完成

--- dist/background.js
+++ dist/background.js
@@ -6,6 +6,11 @@ chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
             Key_KNMTMRNGSTH:localStorage[request.key3],
             Key_KNMTMRNGSTM:localStorage[request.key4],
             Key_RTTLCHKBX56:localStorage[request.key5],
+            Key_KNMTMRNGETD:localStorage[request.key6],
+            Key_KNMTMRNGETH:localStorage[request.key7],
+            Key_KNMTMRNGETM:localStorage[request.key8],
+            Key_RTTLVAL56H :localStorage[request.key9],
+            Key_RTTLVAL56M :localStorage[request.key10],
         });
     else
         sendResponse({});
--- dist/options.js
+++ dist/options.js
@@ -1,8 +1,25 @@
 $(function () {
-    let idList = ["KNMCDS","KNMTMRNGSTD","KNMTMRNGSTH","KNMTMRNGSTM","RTTLCHKBX56","KNMTMRNGETD","KNMTMRNGETH","KNMTMRNGETM","RTTLVAL56H","RTTLVAL56M"];
+    let idList = ["KNMCDS","KNMTMRNGSTD","KNMTMRNGSTH","KNMTMRNGSTM",
+                "KNMTMRNGETD","KNMTMRNGETH","KNMTMRNGETM","RTTLVAL56H","RTTLVAL56M"];
     for(let i = 0; i < idList.length; i++ ){
         $("#" + `${idList[i]}`).val(localStorage.getItem(`${idList[i]}`));
     }
+
+    // Checkbox用
+    let data = localStorage.getItem("RTTLCHKBX56");
+    if (data == 1) {
+        $("input[name='RTTLCHKBX56']").attr("checked", "checked");
+    }
+
+    $("input[name='RTTLCHKBX56']").click(function() {
+        if ($(this).is(":checked")) {
+            localStorage.setItem("RTTLCHKBX56", 1);
+        } else {
+            localStorage.setItem("RTTLCHKBX56", 0);
+
+        }
+    });
+
 
     // 保存ボタン押下時処理
     $("#save").click(function () {
